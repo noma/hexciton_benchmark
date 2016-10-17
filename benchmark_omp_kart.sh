f0b7a2ac3566ac5eae7c1a6ebc2a64d5ce5e3976
noma <ma.noack.pr@gmail.com>
2016-10-17 19:20:33 +0200

    Improved benchmark script to support cray builds.

--- benchmark_omp_kart.sh
+++ benchmark_omp_kart.sh
@@ -5,56 +5,70 @@
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-# Usage: benchmark_omp_kart.sh <result_path>
+# Usage: benchmark_omp_kart.sh <result_path> <-c|-k|-a|"-c -t CC"|"-k -t CC"> <kart-configs...>
 # This will create the folder <result_path> in results containing result files
 # for each run.
 
-RESULT_PATH=./results/$1
-
+# first argument, create result path
+RESULT_PATH=`pwd`/results/$1
 mkdir -p $RESULT_PATH
 
-DEVICES=( "$2" )  # -c -k -a = cpu, knl, knc
-VECLIBS=( "VEC_INTEL" "VEC_VCL" ) # "VEC_INTEL" "VEC_VC" "VEC_VCL"
-KART_CONFIGS=( "intel_16.0.3.kart" "gcc_6.1.0.kart" "clang_3.8.1.kart" )
+# second argument
+MAKE_ARGS=$2 # passed to make_omp_kart.sh
+#derive device name
+DEV_ARG=${2:0:2} # extract first two characters, i.e. "-k" from "-k -t CC"
+case $DEV_ARG in
+-c)
+	DEVICE="cpu"
+	;;
+-k)
+	DEVICE="knl"
+	;;
+-a)
+	DEVICE="knc"
+	;;
+esac
 
-RUNS=25
-ITERATIONS_PER_RUN=105
+# process third argument, copy all specified configs to the result folder
+KART_CONFIGS=""
+for config in ${@:3}
+do
+	# copy the config
+	cp $config $RESULT_PATH/
+	KART_CONFIGS="${RESULT_PATH}/$(basename $config) $KART_CONFIGS"
+done
+echo "Copied all KART configs: $KART_CONFIGS"
+
+# edit this if needed
+VECLIBS=( "VEC_VCL" ) # "VEC_INTEL" "VEC_VC" "VEC_VCL"
+RUNS=20
+ITERATIONS_PER_RUN=55
 WARM_UP_ITERATIONS=5
 
-for device in "${DEVICES[@]}"
+# iterate through vector libs
+for veclib in "${VECLIBS[@]}"
 do
-	for veclib in "${VECLIBS[@]}"
+	BIN_PATH=$RESULT_PATH/bin.${DEVICE}_${veclib}
+	echo "Rebuilding... $BIN_PATH"
+	./make_omp_kart.sh -p $BIN_PATH -v $veclib -i $ITERATIONS_PER_RUN -w $WARM_UP_ITERATIONS $DEV_ARG
+
+	# run through all KART configs, use the copies
+	for config in $KART_CONFIGS
 	do
-		echo "Rebuilding..."
-		./make_omp_kart.sh -v $veclib -i $ITERATIONS_PER_RUN -w $WARM_UP_ITERATIONS $device
-
-		for config in "${KART_CONFIGS[@]}"
+		export KART_DEFAULT_CONFIG=${config} # NOTE: must be absolute path
+		echo "Set KART_DEFAULT_CONFIG to: ${KART_DEFAULT_CONFIG}"
+		# perform multiple runs for statistics
+		for i in `seq 1 ${RUNS}`
 		do
-			export KART_DEFAULT_CONFIG=`pwd`/${config}
-			echo "Set KART_DEFAULT_CONFIG to: ${KART_DEFAULT_CONFIG}"
-			for i in `seq 1 ${RUNS}`
-			do
-				case $device in
-				-c)
-					NAME="cpu"
-					;;
-				-k)
-					NAME="knl"
-					;;
-				-a)
-					NAME="knc"
-					;;
-				esac
+			RUN="./run_${DEVICE}.sh"
+			EXE="$BIN_PATH/benchmark_omp_kart"
 
-				RUN="./run_${NAME}.sh"
-				EXE="bin.${NAME}/benchmark_omp_kart"
-
-				FILE_NAME=${RESULT_PATH}/${NAME}_${veclib}_${config}_run_${i}
-				echo "Benchmarking: ${FILE_NAME}"
-				echo "$RUN $EXE > $FILE_NAME.data 2> $FILE_NAME.log"
-				$RUN $EXE > $FILE_NAME.data 2> $FILE_NAME.log
-			done
+			FILE_NAME=${RESULT_PATH}/${DEVICE}_${veclib}_$(basename $config)_run_${i}
+			echo "Benchmarking: ${FILE_NAME}"
+			echo "$RUN $EXE > $FILE_NAME.data 2> $FILE_NAME.log"
+			$RUN $EXE > $FILE_NAME.data 2> $FILE_NAME.log
 		done
 	done
 done
+
 
--- make_omp_kart.sh
+++ make_omp_kart.sh
@@ -5,12 +5,9 @@
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-CC=icpc
-HAM_PATH=thirdparty/ham
-VC_PATH=thirdparty/vc
-VECCLASS_PATH=thirdparty/vcl
-VECCLASS_PATH_KNC=thirdparty/vcl_KNC
-KART_PATH=thirdparty/kart
+# Usage: make_omp_kart.sh -h
+
+COMPILER=icpc
 
 #VECLIB="VEC_INTEL"
 #VECLIB="VEC_VC"
@@ -18,72 +15,19 @@ VECLIB="VEC_VCL"
 NUM_ITERATIONS=26 # including warmup below
 NUM_WARMUP=1
 
-OPTIONS="-std=c++11 -O3 -restrict -qopenmp -qopt-report=5" #-Wall
-#OPTIONS="-std=c++11 -g -O3 -restrict -openmp -qopt-report=5 -DUSE_INITZERO" #-Wall
-OPTIONS_CPU="$OPTIONS -xHost -DVEC_LENGTH=4"  # -DVC_DOUBLE_V_SIZE=4"
-OPTIONS_KNL="$OPTIONS -xmic-avx512 -DVEC_LENGTH=8" # -DVC_DOUBLE_V_SIZE=8"
-OPTIONS_KNC="$OPTIONS -mmic -DVEC_LENGTH=8"  # -DVC_DOUBLE_V_SIZE=8"
-#OPTIONS_KNC="$OPTIONS_KNC -opt-prefetch-distance=6,1"
-
-INCLUDE="-Iinclude -I${HAM_PATH}/include -I${KART_PATH}/include"
-INCLUDE_CPU="$INCLUDE -I${VC_PATH}/include -I${VECCLASS_PATH} -I${BOOST_ROOT}/include"
-INCLUDE_KNL="$INCLUDE_CPU"
-INCLUDE_KNC="$INCLUDE -I${VC_PATH}/include -I${VECCLASS_PATH_KNC}"
-
-LIB="-lrt"
-#LIB_CPU="$LIB ${VC_PATH}/lib/libVc.a"
-LIB_CPU="$LIB -L${BOOST_ROOT}/lib"
-LIB_KNL="$LIB_CPU"
-LIB_KNC="$LIB ${VC_PATH}/lib/libVc_KNC.a"
-
+BUILD_DIR_ARG="" # unused if empty, defaults below are used
 BUILD_DIR_CPU="bin.cpu"
 BUILD_DIR_KNL="bin.knl"
 BUILD_DIR_KNC="bin.knc"
 
-FILES=( \
-common.cpp \
-kernel/commutator_reference.cpp \
-)
-
-# compile
-build()
-{
-	local BUILD_DIR=$1
-	local OPTIONS=$2
-	local INCLUDE=$3
-	local LIB=$4
-
-	mkdir -p $BUILD_DIR
-			
-	for file in "${FILES[@]}"
-	do
-		local tmp=${file##*/}
-		local NAME=${tmp%.*}
-		$CC -c $OPTIONS $INCLUDE -o ${BUILD_DIR}/${NAME}.o src/${file} & # NOTE: parallel build
-		local OBJS=" $OBJS ${BUILD_DIR}/${NAME}.o"
-	done
-
-	# wait for all build jobs
-	local FAIL_COUNT=0
-	for job in `jobs -p`; do
-		# echo $job
-		wait $job || let "FAIL_COUNT+=1"
-	done
-	if (( FAIL_COUNT > 0 )); then
-		echo "Failed build targets: $FAIL_COUNT"
-	fi
-
-	#echo $OBJS
-
-	$CC $OPTIONS $INCLUDE -o ${BUILD_DIR}/benchmark_omp_kart $OBJS src/benchmark_omp_kart.cpp $LIB ${KART_PATH}/build/libkart.a -lboost_system -lboost_filesystem -lboost_program_options
-}
-
-usage ()
+usage()
 { 
 	echo "Usage and defaults:";
 	echo -e "\t-c\t Build CPU variant.";
 	echo -e "\t-k\t Build KNL variant.";
 	echo -e "\t-a\t Build KNC accelerator variant.";
+	echo -e "\t-p\t Optional build path. ";
+	echo -e "\t-t ${COMPILER}\t Optional compiler: icpc | CC ";
 	echo -e "\t-i ${NUM_ITERATIONS}\t Number of iterations (including warmups).";
 	echo -e "\t-w ${NUM_WARMUP}\t Number of warmup iterations.";
 	echo -e "\t-v ${VECLIB}\t Vector library: VEC_INTEL | VEC_VC | VEC_VCL";
@@ -92,7 +36,7 @@ usage ()
 BUILT_SOMETHING=false
 
 # evaluate command line
-while getopts ":i:w:v:ckah" opt; do
+while getopts ":i:w:v:p:t:ckah" opt; do
 	case $opt in
 	i) # iterations
 		echo "Setting NUM_ITERATIONS to $OPTARG" >&2
@@ -118,6 +62,14 @@ while getopts ":i:w:v:ckah" opt; do
 		echo "Building for KNC accelerator" >&2
 		BUILT_KNC=true
 		;;
+	p) # build path
+		echo "Using build path: $OPTARG" >&2
+		BUILD_DIR_ARG=$OPTARG
+		;;
+	t) # build path
+		COMPILER=$OPTARG
+		echo "Using compiler: $COMPILER" >&2
+		;;
 	h) # usage
 		usage
 		exit 0
@@ -135,21 +87,98 @@ while getopts ":i:w:v:ckah" opt; do
   esac
 done
 
+HAM_PATH=thirdparty/ham
+VC_PATH=thirdparty/vc
+VECCLASS_PATH=thirdparty/vcl
+VECCLASS_PATH_KNC=thirdparty/vcl_KNC
+KART_INCLUDE_PATH=thirdparty/kart
+
+if [[ COMPILER == "CC"  ]]
+then
+	echo "Building with CC..."
+	KART_BUILD_PATH=thirdparty/kart/build_cray
+	OPTIONS="-hstd=c++11 -O3 -hvector3 -hfp3 -hipa5 -hintrinsics -hprototype_intrinsics -homp -hPIC -Drestrict=__restrict"
+	OPTIONS_CPU="$OPTIONS -DVEC_LENGTH=4"
+	OPTIONS_KNL="$OPTIONS -DVEC_LENGTH=8" # -DVC_DOUBLE_V_SIZE=8"
+	OPTIONS_KNC="$OPTIONS -DVEC_LENGTH=8"  # -DVC_DOUBLE_V_SIZE=8"
+	LIB="-lrt -ldl -dynamic"
+else
+	echo "Building with $COMPILER..."
+	KART_BUILD_PATH=thirdparty/kart/build
+	OPTIONS="-std=c++11 -O3 -restrict -qopenmp -qopt-report=5" #-Wall
+	#OPTIONS="-std=c++11 -g -O3 -restrict -openmp -qopt-report=5 -DUSE_INITZERO" #-Wall
+	OPTIONS_CPU="$OPTIONS -xHost -DVEC_LENGTH=4"  # -DVC_DOUBLE_V_SIZE=4"
+	OPTIONS_KNL="$OPTIONS -xmic-avx512 -DVEC_LENGTH=8" # -DVC_DOUBLE_V_SIZE=8"
+	OPTIONS_KNC="$OPTIONS -mmic -DVEC_LENGTH=8"  # -DVC_DOUBLE_V_SIZE=8"
+	LIB="-lrt"
+fi
+
+
+INCLUDE="-Iinclude -I${HAM_PATH}/include -I${KART_INCLUDE_PATH}/include"
+INCLUDE_CPU="$INCLUDE -I${VC_PATH}/include -I${VECCLASS_PATH} -I${BOOST_ROOT}/include"
+INCLUDE_KNL="$INCLUDE_CPU"
+INCLUDE_KNC="$INCLUDE -I${VC_PATH}/include -I${VECCLASS_PATH_KNC}"
+
+#LIB_CPU="$LIB ${VC_PATH}/lib/libVc.a"
+LIB_CPU="$LIB -L${BOOST_ROOT}/lib"
+LIB_KNL="$LIB_CPU"
+LIB_KNC="$LIB ${VC_PATH}/lib/libVc_KNC.a"
+
+FILES=( \
+common.cpp \
+kernel/commutator_reference.cpp \
+)
+
+# compile
+build()
+{
+	local BUILD_DIR=$1
+	local OPTIONS=$2
+	local INCLUDE=$3
+	local LIB=$4
+
+	echo "Creating build path: $BUILD_DIR" >&2
+	mkdir -p $BUILD_DIR
+			
+	for file in "${FILES[@]}"
+	do
+		local tmp=${file##*/}
+		local NAME=${tmp%.*}
+		$COMPILER -c $OPTIONS $INCLUDE -o ${BUILD_DIR}/${NAME}.o src/${file} & # NOTE: parallel build
+		local OBJS="$OBJS ${BUILD_DIR}/${NAME}.o"
+	done
+
+	# wait for all build jobs
+	local FAIL_COUNT=0
+	for job in `jobs -p`; do
+		# echo $job
+		wait $job || let "FAIL_COUNT+=1"
+	done
+	if (( FAIL_COUNT > 0 )); then
+		echo "Failed build targets: $FAIL_COUNT"
+	fi
+
+	#echo $OBJS
+
+	$COMPILER $OPTIONS $INCLUDE -o ${BUILD_DIR}/benchmark_omp_kart $OBJS src/benchmark_omp_kart.cpp $LIB ${KART_BUILD_PATH}/libkart.a -lboost_system -lboost_filesystem -lboost_program_options
+}
+
+
 if [ "$BUILT_CPU" = "true" ]
 then
-	build "$BUILD_DIR_CPU" "$OPTIONS_CPU -DNUM_ITERATIONS=${NUM_ITERATIONS} -DNUM_WARMUP=${NUM_WARMUP} -D${VECLIB}" "$INCLUDE_CPU" "$LIB_CPU"
+	build "${BUILD_DIR_ARG:-$BUILD_DIR_CPU}" "$OPTIONS_CPU -DNUM_ITERATIONS=${NUM_ITERATIONS} -DNUM_WARMUP=${NUM_WARMUP} -D${VECLIB}" "$INCLUDE_CPU" "$LIB_CPU"
 	BUILT_SOMETHING=true
 fi
 
 if [ "$BUILT_KNL" = "true" ]
 then
-	build "$BUILD_DIR_KNL" "$OPTIONS_KNL -DNUM_ITERATIONS=${NUM_ITERATIONS} -DNUM_WARMUP=${NUM_WARMUP} -D${VECLIB}" "$INCLUDE_KNL" "$LIB_KNL"
+	build "${BUILD_DIR_ARG:-$BUILD_DIR_KNL}" "$OPTIONS_KNL -DNUM_ITERATIONS=${NUM_ITERATIONS} -DNUM_WARMUP=${NUM_WARMUP} -D${VECLIB}" "$INCLUDE_KNL" "$LIB_KNL"
 	BUILT_SOMETHING=true
 fi
 
 if [ "$BUILT_KNC" = "true" ]
 then
-	build "$BUILD_DIR_KNC" "$OPTIONS_KNC -DNUM_ITERATIONS=${NUM_ITERATIONS} -DNUM_WARMUP=${NUM_WARMUP} -D${VECLIB}" "$INCLUDE_KNC" "$LIB_KNC"
+	build "${BUILD_DIR_ARG:-$BUILD_DIR_KNC}" "$OPTIONS_KNC -DNUM_ITERATIONS=${NUM_ITERATIONS} -DNUM_WARMUP=${NUM_WARMUP} -D${VECLIB}" "$INCLUDE_KNC" "$LIB_KNC"
 	BUILT_SOMETHING=true
 fi
 
