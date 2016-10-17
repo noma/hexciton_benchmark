9e55c4ede0d131001aca20692bc57a57f5c97452
noma <ma.noack.pr@gmail.com>
2016-10-17 19:19:55 +0200

    Improved run scripts for knl and cpu. They now use portable OpenMP thread pinning, and parse the cpu count from lscpu

--- run_cpu.sh
+++ run_cpu.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-# Copyright (c) 2015 Matthias Noack (ma.noack.pr@gmail.com)
+# Copyright (c) 2015-2016 Matthias Noack (ma.noack.pr@gmail.com)
 #
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-KMP_AFFINITY=granularity=core,compact KMP_BLOCK_TIME=0 `pwd`/$1
+EXE=$1 # NOTE: absolute path
+
+# use optional second argument or 'lscpu' to determine OMP_NUM_THREADS
+THREADS=${2:-$(lscpu | grep "^CPU(s):" | tr -s ' ' | cut -d ' ' -f 2)}
+echo "run_knl.sh: setting OMP_NUM_THREADS to: $THREADS"
+
+unset KMP_AFFINITY
+OMP_NUM_THREADS=$THREADS OMP_PLACES=threads OMP_PROC_BIND=true $EXE
+
+# NOTE: maybe set: KMP_BLOCK_TIME=0 for Intel OMP, see documentation
+#KMP_AFFINITY=granularity=core,compact KMP_BLOCK_TIME=0 $EXE
 
