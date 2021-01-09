060a40c0261356904242b04fc4505be53666cad0
Matthias Noack <ma.noack.pr@gmail.com>
2021-01-09 04:51:20 +0100

    Added new dependency download script.

---
+++ thirdparty/get_dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Copyright (c) 2021 Matthias Noack (ma.noack.pr@gmail.com)
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+git clone https://github.com:noma/ocl.git
+git clone https://github.com:noma/bmt.git
+git clone https://ithub.com:noma/misc.git
+git clone https://ithub.com:noma/typa.git
+git clone https://github.com:noma/kart.git
+git clone https://github.com/vectorclass/version1.git vcl
+
